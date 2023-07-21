b9adb21bb5e3f8d23bf5de891aa94bab2d8cf9e5
ByteSudoer <soussi.mohamednour@gmail.com>
2023-07-21 16:50:23 +0100

    tracing implemented

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use axum::Server;
+use axum::{routing::get, Router};
+use std::env;
+use tracing::*;
+
 #[tokio::main]
 async fn main() {
-    println!("helloo");
+    let env_key = "RUST_LOG";
+    env::set_var(env_key, "trace");
+    tracing_subscriber::fmt::init();
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let server = Server::bind(&"127.0.0.1:3000".parse().unwrap()).serve(app.into_make_service());
+    let addr = server.local_addr();
+    info!("Server listening on {addr}");
+    server.await.unwrap();
 }
