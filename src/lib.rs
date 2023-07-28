dd672b62c89a6b4f9952883ddf9aaddf77f6f882
ByteSudoer <soussi.mohamednour@gmail.com>
2023-07-28 16:53:32 +0100

    load env variables

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,32 @@ pub mod system;
 use routes::create_route;
 use tracing::info;
 
+fn load_address_and_port() -> Result<(String, String), ()> {
+    let address = match dotenv::var("ADDRESS") {
+        Ok(address) => address,
+        Err(e) => {
+            tracing::error!("Error loading ADDRESS env variable from .env : {}", e);
+            return Err(());
+        }
+    };
+
+    let port = match dotenv::var("PORT") {
+        Ok(port) => port,
+        Err(e) => {
+            tracing::error!("Error loading ADDRESS env variable from .env : {}", e);
+            return Err(());
+        }
+    };
+
+    Ok((address, port))
+}
 #[axum::debug_handler]
 pub async fn run() {
     let app = create_route();
+    let bindings = load_address_and_port().unwrap();
+    let url = format!("{}:{}", bindings.0, bindings.1);
 
-    let server =
-        axum::Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(app.into_make_service());
+    let server = axum::Server::bind(&url.parse().unwrap()).serve(app.into_make_service());
     let addr = server.local_addr();
     info!("Server listening on {addr}");
     server.await.unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use dotenv;
 use rust_profiler::run;
 use std::env;
 mod routes;
@@ -7,6 +8,7 @@ fn init() {
     let env_key = "RUST_LOG";
     env::set_var(env_key, "info");
     tracing_subscriber::fmt::init();
+    dotenv::dotenv().ok();
 }
 #[tokio::main]
 async fn main() {
